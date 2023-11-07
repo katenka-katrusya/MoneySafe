5f88c7c9de034cdda1341d6f66aab4cf1dde2fb3
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-11-07 20:10:33 +0300

    дпервый урок

---
+++ js/script.js
@@ -0,0 +1,34 @@
+import {convertStringNumber} from './convertStringNumber.js';
+
+const financeForm = document.querySelector('.finance__form');
+const financeAmount = document.querySelector('.finance__amount');
+const financeReportBtn = document.querySelector('.finance__report');
+let amount = 0;
+
+financeAmount.textContent = amount;
+
+financeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const typeOperation = e.submitter.dataset.typeOperation;
+    const changeAmount = Math.abs(convertStringNumber(financeForm.amount.value));
+    console.log(changeAmount);
+    if (typeOperation === 'income') {
+        amount += changeAmount;
+    } else if (typeOperation === 'expenses') {
+        amount -= changeAmount;
+    }
+
+    // financeAmount.textContent = `${amount.toLocaleString()} ₽`;
+    console.log(`${amount.toLocaleString()} ₽`);
+});
+
+financeReportBtn.addEventListener('click', () => {
+    const report = document.querySelector('.report');
+    report.classList.add('report__open');
+
+    const reportClose = report.querySelector('.report__close');
+    reportClose.addEventListener('click', () => {
+        report.classList.remove('report__open');
+    });
+});
+
